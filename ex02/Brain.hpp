a8a68388db30106cdb9d8fc712f430c182f3ea70
Lou-Anne Buisson <louanne.buisson@gmail.com>
2025-06-13 10:34:09 +0200

    CPP04: header added

--- ex02/Brain.hpp
+++ ex02/Brain.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbuisson <lbuisson@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/13 10:32:01 by lbuisson          #+#    #+#             */
+/*   Updated: 2025/06/13 10:32:09 by lbuisson         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BRAIN_H
 # define BRAIN_H
 
@@ -14,7 +26,7 @@ class Brain {
 		std::string getIdea(int index) const;
 		void setIdea(int index, std::string add);
 
-	private: // protected ??
+	private:
 		std::string ideas[100];
 };
 
